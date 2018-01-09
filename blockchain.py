ec254d12145da60eb6748fdb648ab644d50f6d4f
Vinayak <vinayakkaushikk@gmail.com>
2018-01-09 13:00:04 +0530

    Blockchain mostly implemented

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,8 @@ import json
 
 from time import time
 from uuid import uuid4
+from textwrap import dedent
+from flask import Flask
 
 
 class Blockchain(object):
@@ -101,3 +103,44 @@ class Blockchain(object):
         return guess_hash[:4] == "0000"
 
 
+# Instantiate our node
+
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate our Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transactions():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': 'Transaction will be added to Block {}'.format(index)}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+            'chain': blockchain.chain,
+            'length': len(blockchain.chain),
+            }
+
+    return jsonify(response), 200
+
+
+if __name__== '__main__':
+    app.run(host='0.0.0.0', port=5000)
